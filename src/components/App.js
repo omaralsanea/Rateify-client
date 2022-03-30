89af4b6898eb86f9b1c262d1b52808ac1560fb18
JT Black <87031124+jacktblack@users.noreply.github.com>
2022-03-30 10:48:04 +0100

    carrousel added

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
 import Home from './Home';
 import ShowRelease from './ShowRelease';
 import Navbar from './Navbar';
@@ -18,15 +17,15 @@ const App = () => (
   <BrowserRouter>
     <Navbar />
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/releases/:id" element={<ShowRelease />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/create" element={<NewRelease />} />
-      <Route path="/artists" element={<ArtistIndex />} />
-      <Route path="/addartist" element={<AddArtist />} />
-      <Route path="/releases" element={<ReleaseIndex />} />
-      <Route path="/addrelease" element={<AddRelease />} />
+      <Route path='/' element={<Home />} />
+      <Route path='/releases/:id' element={<ShowRelease />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
+      <Route path='/create' element={<NewRelease />} />
+      <Route path='/artists' element={<ArtistIndex />} />
+      <Route path='/addartist' element={<AddArtist />} />
+      <Route path='/releases' element={<ReleaseIndex />} />
+      <Route path='/addrelease' element={<AddRelease />} />
     </Routes>
   </BrowserRouter>
 );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,46 +1,61 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import ReleaseCard from './release/ReleaseCard';
+import Carousel from './Carousel';
+
 import { getAllReleases } from '../api/release';
 
 const Home = () => {
-  // const [releases, setReleases] = React.useState(null);
+  const [releases, setReleases] = React.useState(null);
 
-  // React.useEffect(() => {
-  //   const getData = async () => {
-  //     const release = await getAllReleases();
-  //     setReleases(release);
-  //   };
-
-  //   getData();
-  // }, []);
-
-  // console.log('RELEASES ARE', releases);
+  React.useEffect(() => {
+    const getData = async () => {
+      try {
+        const data = await getAllReleases();
+        setReleases(data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    getData();
+  }, []);
 
   return (
-    // <>
-    //   <h1 className="title">Music Rating</h1>
-    //   {releases ? (
-    //     <div className="container">
-    //       <div className="columns">
-    //         {releases.map((release) => (
-    //           <div key={release._id} className="column card m-4">
-    //             <Link to={`/release/${release._id}`}>
-    //               <h2 className="card-header">title: {release.title}</h2>
-    //               <div className="card-image">
-    //                 <figure className="image is-3by4">
-    //                   <img src={release.img} alt={release.title} />
-    //                 </figure>
-    //               </div>
-    //             </Link>
-    //           </div>
-    //         ))}
-    //       </div>
-    //     </div>
-    //   ) : (
-    //     <p>Music Loading...</p>
-    //   )}
-    // </>
-    <p>Welcome to the Music Rating App</p>
+    <>
+      <h1 className='title is-3 has-text-centered'>
+        Welcome to the Music Rating App!
+      </h1>
+
+      {releases && <Carousel releases={releases} />}
+      {/* <h2 className='title is-4 has-text-centered'>Latest Releases</h2>
+      <section className='section'>
+        <div className='container'>
+          <div className='columns is-flex-direction-row'>
+            {!releases ? (
+              <p>Loading Releases...</p>
+            ) : (
+              releases.map((release) => (
+                <ReleaseCard key={release._id} {...release} />
+              ))
+            )}
+          </div>
+        </div>
+      </section> */}
+      <h2 className='title is-4 has-text-centered'>Highest Rated Releases</h2>
+      <section className='section'>
+        <div className='container'>
+          <div className='columns is-flex-direction-row'>
+            {!releases ? (
+              <p>Loading Releases...</p>
+            ) : (
+              releases.map((release) => (
+                <ReleaseCard key={release._id} {...release} />
+              ))
+            )}
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
